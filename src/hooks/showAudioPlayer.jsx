7bcd0a089c1e088bf6de387bdeadc9ea6bf2c9e7
JK <jk.floods@gmail.com>
2023-03-29 23:54:22 +0200

    plyr react

--- src/hooks/showAudioPlayer.jsx
+++ src/hooks/showAudioPlayer.jsx
@@ -1,15 +1,30 @@
+import Plyr from 'plyr-react';
+import 'plyr-react/plyr.css';
+
 /**
  * Player audio
  */
 export default function showAudioPlayer(googleId) {
   return (
-    <audio
-      className="player__audio"
-      controls
-      autoPlay
-      src={`https://drive.google.com/uc?id=${googleId}`}
-    >
-      Your browser does not support the audio element.
-    </audio>
+    <Plyr
+      source={{
+        type: 'audio',
+        sources: [{ src: `https://drive.google.com/uc?id=${googleId}` }],
+      }}
+      options={{
+        controls: [
+          'play-large',
+          'play',
+          'progress',
+          'current-time',
+          'mute',
+          'volume',
+          'settings',
+        ],
+        settings: ['speed'],
+        autoplay: true,
+        speed: { selected: 1, options: [0.5, 0.75, 1] },
+      }}
+    />
   );
 }

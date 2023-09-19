90853e974f33e16f2aabef42ca7e66b24cc3d56b
JK <jk.floods@gmail.com>
2023-09-20 00:30:04 +0200

    Add first tutos

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Lyrics from './pages/Lyrics';
 import Error from './pages/Error';
+import './css/reset.css';
 import './css/style.css';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Lyrics />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Lyrics />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/pages/Lyrics.jsx
+++ src/pages/Lyrics.jsx
@@ -1,5 +1,53 @@
+import React from 'react';
+import songs from '../data/songs';
+import playButton from '../assets/play.svg';
+import driveButton from '../assets/drive.png';
+// import lyricsButton from '../assets/text.png';
+
 function Lyrics() {
-  return <div>Lyrics</div>;
+  return (
+    <section className="section">
+      <h1 className="section__title">Tutoriels</h1>
+      <ul className="songs">
+        {songs.map((song) => (
+          <li className="song">
+            <a className="song__title" href={`#song_${song.id}`}>
+              {song.title} - {song.artist}
+            </a>
+            <div className="song__content" id={`song_${song.id}`}>
+              {song.tutorials.map((tutorial) => (
+                <div className={`tutorial ${tutorial.icon}`} id={tutorial.id}>
+                  <div className="tutorial__heading">
+                    {tutorial.title}
+                    <div className="tutorial__heading_buttons">
+                      <button type="button" className="btn-play-tutorial">
+                        <img
+                          src={playButton}
+                          alt="Jouer"
+                          className="tutorial__heading__icon tutorial__heading__icon--play"
+                        />
+                      </button>
+                      <button
+                        type="button"
+                        href="https://drive.google.com/file/d/1W5aEQezoKCQ766x4ULXc7oKyxoMoWrcB/view?usp=sharing"
+                        className="btn-download-tutorial"
+                      >
+                        <img
+                          src={driveButton}
+                          alt="Drive"
+                          className="tutorial__heading__icon tutorial__heading__icon--download"
+                        />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default Lyrics;

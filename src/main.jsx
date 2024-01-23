452e5e73067b569eeb6f25a9261a95e2f464008c
JK <jk.floods@gmail.com>
2024-01-23 18:06:25 +0100

    CONFIG FESTIROCK

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/festirock2">
       <Provider store={store}>
         <App />
       </Provider>

8a99f5a157d5be054a9b609a43c11dd4f6d74936
JK <jk.floods@gmail.com>
2024-02-08 02:55:20 +0100

    Defining custom elements

--- src/components/SlideEditor.jsx
+++ src/components/SlideEditor.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
-import { createEditor } from 'slate';
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import { useState, useCallback } from 'react';
+import { Editor, Transforms, createEditor, Element } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 
 const initialValue = [
@@ -9,14 +12,54 @@ const initialValue = [
   },
 ];
 
-function Editor() {
+function DefaultElement(props) {
+  return <p {...props.attributes}>{props.children}</p>;
+}
+
+function CodeElement(props) {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+}
+
+function SlideEditor() {
   const [editor] = useState(() => withReact(createEditor()));
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
 
   return (
     <Slate editor={editor} initialValue={initialValue}>
-      <Editable />
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === 'c' && event.ctrlKey && event.altKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault();
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === 'code',
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              {
+                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
+              },
+            );
+          }
+        }}
+      />
     </Slate>
   );
 }
 
-export default Editor;
+export default SlideEditor;

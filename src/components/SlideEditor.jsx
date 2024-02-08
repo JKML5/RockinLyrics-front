a4383c9e1a9c779ba24392aa5c86ad75e04ad192
JK <jk.floods@gmail.com>
2024-02-09 00:12:12 +0100

    Clear useless code

--- src/components/SlideEditor.jsx
+++ src/components/SlideEditor.jsx
@@ -2,22 +2,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import { useState, useCallback } from 'react';
-import { Editor, Transforms, createEditor, Element } from 'slate';
+import { Editor, createEditor } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 
-// Define our own custom set of helpers.
 const CustomEditor = {
   isBoldMarkActive(editor) {
     const marks = Editor.marks(editor);
     return marks ? marks.bold === true : false;
-  },
-
-  isCodeBlockActive(editor) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.type === 'code',
-    });
-
-    return !!match;
   },
 
   toggleBoldMark(editor) {
@@ -28,38 +19,7 @@ const CustomEditor = {
       Editor.addMark(editor, 'bold', true);
     }
   },
-
-  toggleCodeBlock(editor) {
-    const isActive = CustomEditor.isCodeBlockActive(editor);
-
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? 'paragraph' : 'code' },
-      {
-        match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
-      },
-    );
-  },
 };
-
-const initialValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
-
-function DefaultElement(props) {
-  return <p {...props.attributes}>{props.children}</p>;
-}
-
-function CodeElement(props) {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
-}
 
 function Leaf(props) {
   return (
@@ -74,15 +34,14 @@ function Leaf(props) {
 
 function SlideEditor() {
   const [editor] = useState(() => withReact(createEditor()));
+  const initialText = 'A line of text in a paragraph.';
 
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
+  const initialValue = [
+    {
+      type: 'paragraph',
+      children: [{ text: initialText }],
+    },
+  ];
 
   // Define a leaf rendering function that is memoized with `useCallback`.
   const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
@@ -99,28 +58,12 @@ function SlideEditor() {
         >
           Bold
         </button>
-        <button
-          type="button"
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleCodeBlock(editor);
-          }}
-        >
-          Code Block
-        </button>
       </div>
       <Editable
-        renderElement={renderElement}
         renderLeaf={renderLeaf}
         onKeyDown={(event) => {
-          // Switch normal / code mode
-          if (event.key === 'c' && event.ctrlKey && event.altKey) {
-            event.preventDefault();
-            CustomEditor.toggleCodeBlock(editor);
-          }
-
           // Bold
-          else if (event.key === 'b' && event.ctrlKey) {
+          if (event.key === 'b' && event.ctrlKey) {
             event.preventDefault();
             CustomEditor.toggleBoldMark(editor);
           }

59759ad4aa9d8dfad4524e377d71fbf761a6ebff
JK <jk.floods@gmail.com>
2024-06-21 01:02:17 +0200

    rm /riv3

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/riv3">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/riv3',
   plugins: [react()],
 });
